533fe47231f7ba1e0d05b0f8b41f408cca1a953e
Willy Tarreau <w@1wt.eu>
2021-03-08 14:38:44 +0100

    tools: add a git submodule and build script for h1load
    It's another easy to build client that's not available by default in
    distros and which provides live stats and focuses on latency accuracy.

---
+++ tools/clients/h1load/build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+run() {
+  echo "  RUN   $*"
+  command "$@"
+}
+
+if [ ! -e "source/Makefile" ]; then
+  echo "h1load source not found in 'source', trying to update submodule"
+  run git submodule update source
+  if [ ! -e "source/Makefile" ]; then
+    echo "Couldn't retrieve usable h1load source. Please try to fix the condition"
+    echo "and run 'git submodule update source' from this directory."
+    exit 1
+  fi
+fi
+
+nproc=$(nproc 2>/dev/null)
+[ -n "$nproc" -a -z "${nproc##[0-9]*}" ] || nproc=1
+
+cd source
+if make -j$nproc && mkdir -p ../bin && cp -v h1load ../bin/; then
+  make clean
+  cd ..
+  echo
+  echo "Done! The h1load executable is now in $PWD/bin"
+  exit 0
+else
+  echo
+  echo "Failed! Please try to fix build errors and start again, or copy the"
+  echo "'h1load' executable into bin/"
+  exit 1
+fi
+
